80798542d604b5b8d5b45e7337206952070f5e8d
Brent Vatne <brentvatne@gmail.com>
2023-06-28 12:04:53 -0700

    example

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,13 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+
+function callback() {
+  console.log("breakpoint");
+}
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <Button title="Another one" onPress={callback} />
+      <Button title="Press me" onPress={callback} />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
@@ -16,16 +26,16 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
